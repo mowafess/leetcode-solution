c4034849ed7a1f4369e3d2e6c3bc40aca7d7bb56
mowafess <36759376+mowafess@users.noreply.github.com>
2021-12-27 22:40:49 +0000

    Time: 40 ms (87.59%), Space: 14.6 MB (32.28%) - LeetHub

---
+++ valid-anagram/valid-anagram.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        # if len(s) != len(t) or set(s) != set(t):
+        #     return False
+        
+        # freqTable = defaultdict(int)
+
+        # for i in range(len(s)):
+        #     freqTable[s[i]] += 1
+        #     freqTable[t[i]] -= 1
+
+        # for k, v in freqTable.items():
+        #     if v != 0:
+        #         return False
+
+        # return True
+
+        if len(s) != len(t) or set(s) != set(t):
+            return False  
+        return (Counter(s) - Counter(t)) == Counter()
