71a3592b7b879f87d40dfa0964f26ed4791d27aa
mowafess <36759376+mowafess@users.noreply.github.com>
2022-01-05 20:07:04 +0000

    Time: 279 ms (16.81%), Space: 14.8 MB (46.12%) - LeetHub

--- 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.py
+++ 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.py
@@ -1,21 +1,30 @@
-class Solution:
-    def maximumUnits(self, boxTypes: List[List[int]], truckSize: int) -> int:
-        boxTypes = sorted(boxTypes, key=lambda x: -x[1])
-        box = sum(box[0] for box in boxTypes)
-        unit = sum(box[0] * box[1] for box in boxTypes)
+# class Solution:
+#     def maximumUnits(self, boxTypes: List[List[int]], truckSize: int) -> int:
+#         boxTypes = sorted(boxTypes, key=lambda x: -x[1])
+# #         box = sum(box[0] for box in boxTypes)
+# #         unit = sum(box[0] * box[1] for box in boxTypes)
         
-        i = result = 0
+#         i = result = 0
         
-        if truckSize > box:
-            result, truckSize = divmod(box, truckSize)
-            result *= unit
+#         # if truckSize > box:
+#         #     result, truckSize = divmod(box, truckSize)
+#         #     result *= unit
         
-        while truckSize > 0:
-            num = min(boxTypes[i][0], truckSize)
-            result += num * boxTypes[i][1]
-            truckSize -= num
-            i += 1
+#         while truckSize > 0:
+#             num = min(boxTypes[i][0], truckSize)
+#             result += num * boxTypes[i][1]
+#             truckSize -= num
+#             i += 1
         
             
-        return result
-        
+#         return result
+
+class Solution:
+    def maximumUnits(self, boxTypes: List[List[int]], truckSize: int) -> int:
+        boxTypes.sort(key=lambda x: -x[1])
+        cur_size = 0
+        max_units = 0
+        for num_box, unit in boxTypes:
+            max_units += unit * min(truckSize - cur_size, num_box)
+            cur_size += min(truckSize - cur_size, num_box)
+        return max_units
