5d5f5b30ae9c12507d82bd697c8465f1bab91785
mowafess <36759376+mowafess@users.noreply.github.com>
2022-01-24 19:08:20 +0000

    Time: 813 ms (40.85%), Space: 36.3 MB (27.56%) - LeetHub

--- 295-find-median-from-data-stream/295-find-median-from-data-stream.py
+++ 295-find-median-from-data-stream/295-find-median-from-data-stream.py
@@ -1,24 +1,47 @@
-# from heapq import *
-from bisect import insort
+from heapq import *
 
 class MedianFinder:
     
-    # another log N alternative... use insort with one array
-
     def __init__(self):
-        self.store = []
+        self.max_heap = []
+        self.min_heap = []
 
     def addNum(self, num: int) -> None:
-        # Keep in mind that the O(log n) search is dominated by the slow O(n) insertion step.
-        insort(self.store, num)
+        if not self.max_heap or num >= self.max_heap[0]:
+            heappush(self.max_heap, num)
+        else:
+            heappush(self.min_heap, -num)
+            
+        if len(self.max_heap) - len(self.min_heap) > 1:
+            heappush(self.min_heap, -heappop(self.max_heap))
+        elif len(self.max_heap) < len(self.min_heap):
+            heappush(self.max_heap, -heappop(self.min_heap))   
 
-    def findMedian(self) -> float:
-        mid = len(self.store) // 2
+    def findMedian(self) -> float:   
+        if len(self.max_heap) == len(self.min_heap):
+            return (self.max_heap[0] - self.min_heap[0]) / 2
         
-        if len(self.store) % 2:
-            return self.store[mid]
+        return self.max_heap[0]
+
+
+# class MedianFinder:
+    
+#     # another log N alternative... use insort with one array
+
+#     def __init__(self):
+#         self.store = []
+
+#     def addNum(self, num: int) -> None:
+#         # Keep in mind that the O(log n) search is dominated by the slow O(n) insertion step.
+#         insort(self.store, num)
+
+#     def findMedian(self) -> float:
+#         mid = len(self.store) // 2
         
-        return (self.store[mid-1] + self.store[mid]) / 2
+#         if len(self.store) % 2:
+#             return self.store[mid]
+        
+#         return (self.store[mid-1] + self.store[mid]) / 2
         
 
 
