1ab206d413ef44414bdbffcdb8cdf17df13864f6
mowafess <36759376+mowafess@users.noreply.github.com>
2022-01-25 20:34:27 +0000

    Time: 1070 ms (24.34%), Space: 19.9 MB (49.81%) - LeetHub

--- 973-k-closest-points-to-origin/973-k-closest-points-to-origin.py
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.py
@@ -1,20 +1,16 @@
+from heapq import *
+
 class Solution:
     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
-        # Since heap is sorted in increasing order,
-        # negate the distance to simulate max heap
-        # and fill the heap with the first k elements of points
-        heap = [(-self.squared_distance(points[i]), i) for i in range(k)]
-        heapq.heapify(heap)
-        for i in range(k, len(points)):
-            dist = -self.squared_distance(points[i])
-            if dist > heap[0][0]:
-                # If this point is closer than the kth farthest,
-                # discard the farthest point and add this one
-                heapq.heappushpop(heap, (dist, i))
+        store = []
         
-        # Return all points stored in the max heap
-        return [points[i] for (_, i) in heap]
-    
-    def squared_distance(self, point: List[int]) -> int:
-        """Calculate and return the squared Euclidean distance."""
-        return point[0] ** 2 + point[1] ** 2
+        for i, point in enumerate(points):
+            heappush(store, (math.hypot(*point), points[i]))
+            
+        output = []
+        while k > 0:
+            dist, point = heappop(store)
+            output.append(point)
+            k -= 1
+            
+        return output
