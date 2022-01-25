3b6bb633c16f61d8be2aed5869b9e20ab63602cc
mowafess <36759376+mowafess@users.noreply.github.com>
2022-01-25 20:42:49 +0000

    Time: 748 ms (50.76%), Space: 20 MB (39.08%) - LeetHub

--- 973-k-closest-points-to-origin/973-k-closest-points-to-origin.py
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.py
@@ -1,5 +1,22 @@
-from math import hypot
+from heapq import heappush, heappop
 
 class Solution:
-    def kClosest(self, points: List[List[int]], K: int) -> List[List[int]]:
-        return sorted(points, key=lambda point: hypot(point[0], point[1]))[:K]
+    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+        store = []
+        
+        for i, point in enumerate(points):
+            heappush(store, (math.hypot(*point), points[i]))
+            
+        output = []
+        while k > 0:
+            dist, point = heappop(store)
+            output.append(point)
+            k -= 1
+            
+        return output
+
+# from math import hypot
+
+# class Solution:
+#     def kClosest(self, points: List[List[int]], K: int) -> List[List[int]]:
+#         return sorted(points, key=lambda point: abs(hypot(point[0], point[1])))[:K]
