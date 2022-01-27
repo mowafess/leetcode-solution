87096e309e436fda439912cd438371c0a23a17f5
mowafess <36759376+mowafess@users.noreply.github.com>
2022-01-27 22:46:47 +0000

    Time: 28 ms (90.41%), Space: 13.9 MB (99.96%) - LeetHub

---
+++ 362-design-hit-counter/362-design-hit-counter.py
@@ -0,0 +1,28 @@
+from collections import deque
+from bisect import bisect_right
+
+class HitCounter:
+    
+    def __init__(self):
+        self.hits = deque([])
+
+    def hit(self, timestamp: int) -> None:
+        self.hits.append(timestamp)
+        start = self.hits[0]
+        
+        while timestamp - 300 > start:
+            self.hits.popleft()
+            start = self.hits[0]
+        
+
+    def getHits(self, timestamp: int) -> int:
+        limit = len(self.hits)
+        curr = bisect_right(self.hits, timestamp - 300)
+        return limit - curr
+        
+
+
+# Your HitCounter object will be instantiated and called as such:
+# obj = HitCounter()
+# obj.hit(timestamp)
+# param_2 = obj.getHits(timestamp)
