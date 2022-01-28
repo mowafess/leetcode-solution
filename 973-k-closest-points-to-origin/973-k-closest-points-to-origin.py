cfc587a8e73cd031855d60b486465b3e981a9760
mowafess <36759376+mowafess@users.noreply.github.com>
2022-01-28 00:01:28 +0000

    Time: 776 ms (48.72%), Space: 20.6 MB (12.04%) - LeetHub

--- 973-k-closest-points-to-origin/973-k-closest-points-to-origin.py
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.py
@@ -1,19 +1,22 @@
-from heapq import heappush, heappop
+from heapq import heappush, heappushpop, heapify
 
 class Solution:
     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
-        store = []
+        store = [(-math.hypot(*points[i]), i) for i in range(k)]
+        heapify(store)
         
-        for i, point in enumerate(points):
-            heappush(store, (math.hypot(*point), point))
+        for i in range(k, len(points)):
+            dist = -math.hypot(*points[i])
+            if dist > store[0][0]:
+                heappushpop(store, (dist, i))
             
-        output = []
-        while k > 0:
-            dist, point = heappop(store)
-            output.append(point)
-            k -= 1
+        # output = []
+        # while k > 0:
+        #     dist, point = heappop(store)
+        #     output.append(point)
+        #     k -= 1
             
-        return output
+        return [points[i]for _, i in store]
 
 # from math import hypot
 
