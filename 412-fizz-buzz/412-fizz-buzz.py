c4195024128513ee1bba9915bc74dee85ee56105
mowafess <36759376+mowafess@users.noreply.github.com>
2022-02-09 21:01:16 +0000

    Time: 48 ms (60.76%), Space: 15 MB (62.83%) - LeetHub

--- 412-fizz-buzz/412-fizz-buzz.py
+++ 412-fizz-buzz/412-fizz-buzz.py
@@ -1,11 +1,19 @@
 class Solution:
     def fizzBuzz(self, n: int) -> List[str]:
         
-        i = 1
         output = []
-        while i <= n:
-            output.append("FizzBuzz" if i % 15 == 0 else "Fizz" if i % 3 == 0 else "Buzz" if i % 5 == 0 else str(i))
-            i += 1
+        
+        for i in range(1, n + 1):
+            curr = str(i)
+            
+            if not i % 15:
+                curr = "FizzBuzz"
+            elif not i % 5:
+                curr = "Buzz"
+            elif not i % 3:
+                curr = "Fizz"
+                
+            output.append(curr)
             
         return output
             
