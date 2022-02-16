9963656521d50153cd2541620dccdcaf090cc7a6
mowafess <36759376+mowafess@users.noreply.github.com>
2022-02-16 21:24:17 +0000

    Time: 151 ms (71.74%), Space: 13.9 MB (93.93%) - LeetHub

--- 647-palindromic-substrings/647-palindromic-substrings.py
+++ 647-palindromic-substrings/647-palindromic-substrings.py
@@ -1,16 +1,40 @@
 class Solution:
     def countSubstrings(self, s: str) -> int:
+#         O(N^3); O(1)
+#         def isPalindrome(word):
+#             return word == word[::-1]
         
-        def isPalindrome(word):
-            return word == word[::-1]
+#         size = len(s)
+#         count = 0
         
+#         for i in range(size):
+#             for j in range(i+1, size+1):
+#                 if isPalindrome(s[i:j]):
+#                     count += 1
+
+#         return count
+
+        
+        res = 0
         size = len(s)
-        count = 0
+        
+        def countPalindrome(i, odd):
+            nonlocal res
+            l = i
+            r = i if odd else i + 1
+            
+            while l >= 0 and r < size and s[l] == s[r]:
+                res += 1
+                l -= 1
+                r += 1
         
         for i in range(size):
-            for j in range(i+1, size+1):
-                if isPalindrome(s[i:j]):
-                    count += 1
-
-        return count
+            countPalindrome(i, True)
+            countPalindrome(i, False)
+            
+        return res
+            
+            
+        
+    
         
