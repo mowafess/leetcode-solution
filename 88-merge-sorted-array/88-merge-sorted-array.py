462b1658272a6132f2c7e1583115f2b4dc65e96f
mowafess <36759376+mowafess@users.noreply.github.com>
2022-02-17 12:26:25 +0000

    Time: 49 ms (51.73%), Space: 13.9 MB (80.89%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.py
+++ 88-merge-sorted-array/88-merge-sorted-array.py
@@ -27,18 +27,17 @@ class Solution:
 #                 p2 -= 1
 
         last = m + n - 1
-        i1, i2 = m, n
         
-        while i1 > 0 and i2 > 0:
-            if nums1[i1 - 1] > nums2[i2 - 1]:
-                nums1[last] = nums1[i1 - 1]
-                i1 -= 1
+        while m > 0 and n > 0:
+            if nums1[m - 1] > nums2[n - 1]:
+                nums1[last] = nums1[m - 1]
+                m -= 1
             else:
-                nums1[last] = nums2[i2 - 1]
-                i2 -= 1
+                nums1[last] = nums2[n - 1]
+                n -= 1
             last -= 1
             
-        while i2 > 0:
-            nums1[last] = nums2[i2 - 1]
-            i2 -= 1
+        while n > 0:
+            nums1[last] = nums2[n - 1]
+            n -= 1
             last -= 1
