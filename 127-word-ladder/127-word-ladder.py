2b86ec438978a42897bb513689fc274dd7ba4874
mowafess <36759376+mowafess@users.noreply.github.com>
2022-02-20 16:48:45 +0000

    Time: 257 ms (58.32%), Space: 19.8 MB (9.19%) - LeetHub

---
+++ 127-word-ladder/127-word-ladder.py
@@ -0,0 +1,36 @@
+class Solution:
+    def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
+        
+        def generate_graph(word_list):
+            graph = defaultdict(set)
+            
+            for word in word_list:
+                for i in range(len(word)):
+                        graph[word[:i] + "*" + word[i+1:]].add(word)
+
+            return graph
+        
+        def word_ladder(begin: str, end: str, word_list: List[str]) -> int:
+            if not word_list or not begin or not end or end not in word_list:
+                return 0
+            
+            graph = generate_graph(word_list)
+            # print(graph)
+            q = deque([(begin, 1)])
+            visited = set(begin)
+
+            while q:
+                node, level = q.popleft()
+                
+                for i in range(len(node)):
+                    search = node[:i] + "*" + node[i+1:]
+                    for nei in graph[search]:
+                        if nei == end:
+                            return level + 1
+                        if nei not in visited:
+                            q.append((nei, level + 1))
+                            visited.add(nei)
+
+            return 0
+        # print(sorted(wordList))
+        return word_ladder(beginWord, endWord, wordList)
