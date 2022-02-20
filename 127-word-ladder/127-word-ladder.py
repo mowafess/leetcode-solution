6feabfe4fc1040f09d7d941d525f8c673846224a
mowafess <36759376+mowafess@users.noreply.github.com>
2022-02-20 16:55:06 +0000

    Time: 943 ms (13.73%), Space: 15.2 MB (72.53%) - LeetHub

--- 127-word-ladder/127-word-ladder.py
+++ 127-word-ladder/127-word-ladder.py
@@ -1,3 +1,5 @@
+from string import ascii_letters
+
 class Solution:
     def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
         
@@ -14,7 +16,7 @@ class Solution:
             if not word_list or not begin or not end or end not in word_list:
                 return 0
             
-            graph = generate_graph(word_list)
+            graph = set(word_list)
 
             q = deque([(begin, 1)])
             visited = set(begin)
@@ -23,13 +25,14 @@ class Solution:
                 node, level = q.popleft()
                 
                 for i in range(len(node)):
-                    search = node[:i] + "*" + node[i+1:]
-                    for nei in graph[search]:
-                        if nei == end:
-                            return level + 1
-                        if nei not in visited:
-                            q.append((nei, level + 1))
-                            visited.add(nei)
+                    for c in ascii_letters:
+                        nei = node[:i] + c + node[i+1:]
+                        if nei in graph:
+                            if nei == end:
+                                return level + 1
+                            if nei not in visited:
+                                q.append((nei, level + 1))
+                                visited.add(nei)
 
             return 0
         
