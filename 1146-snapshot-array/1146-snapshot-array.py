75dd199334f9519422ce4e9f48d5239aca3d43c5
mowafess <36759376+mowafess@users.noreply.github.com>
2022-02-20 13:33:12 +0000

    Time: 396 ms (92.84%), Space: 33.3 MB (76.01%) - LeetHub

--- 1146-snapshot-array/1146-snapshot-array.py
+++ 1146-snapshot-array/1146-snapshot-array.py
@@ -1,20 +1,44 @@
+from collections import defaultdict
 class SnapshotArray:
 
     def __init__(self, length: int):
-        self.curr = [0] * length
-        self.history = []
-        self.max_length = 0
-        
+        self.length = length
+        self.id = 0
+        self.snapshots = defaultdict(dict)
+
     def set(self, index: int, val: int) -> None:
-        self.curr[index] = val
-        self.max_length = max(self.max_length, index + 1)
-        
+        self.snapshots[self.id][index] = val
+
     def snap(self) -> int:
-        self.history.append(self.curr[:self.max_length])
-        return len(self.history) - 1
+        current_id = self.id
+        self.id += 1
+        return current_id
 
     def get(self, index: int, snap_id: int) -> int:
-        return 0 if index > len(self.history[snap_id]) - 1 else self.history[snap_id][index]
+        while snap_id >=0:
+            for snap_index in self.snapshots[snap_id]:
+                if index == snap_index:
+                    return self.snapshots[snap_id][snap_index]
+            snap_id -= 1
+        return 0
+
+# class SnapshotArray:
+
+#     def __init__(self, length: int):
+#         self.curr = [0] * length
+#         self.history = []
+#         self.max_length = 0
+        
+#     def set(self, index: int, val: int) -> None:
+#         self.curr[index] = val
+#         self.max_length = max(self.max_length, index + 1)
+        
+#     def snap(self) -> int:
+#         self.history.append(self.curr[:self.max_length])
+#         return len(self.history) - 1
+
+#     def get(self, index: int, snap_id: int) -> int:
+#         return 0 if index > len(self.history[snap_id]) - 1 else self.history[snap_id][index]
         
 
 
