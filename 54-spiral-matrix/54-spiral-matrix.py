86c356150a168b914758a7debceb1a7f92218aaf
mowafess <36759376+mowafess@users.noreply.github.com>
2022-03-04 06:43:07 +0000

    Time: 71 ms (5.43%), Space: 14 MB (63.99%) - LeetHub

--- 54-spiral-matrix/54-spiral-matrix.py
+++ 54-spiral-matrix/54-spiral-matrix.py
@@ -4,7 +4,6 @@ class Solution:
         res = []
         
         top, left, right, bottom = 0, 0, len(matrix[0]), len(matrix)
-        0, 0, 3, 3
         
         while top < bottom and left < right:
             col = left
@@ -12,30 +11,27 @@ class Solution:
                 res.append(matrix[top][col])
                 col += 1
             top += 1
-            print(1, res)
             
             row = top
             while row < bottom:
                 res.append(matrix[row][right-1])
                 row += 1
             right -= 1
-            print(2, res)
             
-            if not(left < right and top < bottom): break
+            if not(left < right and top < bottom): 
+                break
             
             col = right
             while col > left:
                 res.append(matrix[bottom-1][col-1])
                 col -= 1
             bottom -= 1
-            print(3, res)
             
             row = bottom
             while row > top:
                 res.append(matrix[row-1][left])
                 row -= 1
             left += 1
-            print(4, res)
             
         return res
             
