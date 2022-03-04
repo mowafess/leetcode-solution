3581fdbc40759a1a05685b5cf42af14c5914453d
mowafess <36759376+mowafess@users.noreply.github.com>
2022-03-04 05:30:40 +0000

    Time: 53 ms (24.59%), Space: 14 MB (55.55%) - LeetHub

--- 500-keyboard-row/500-keyboard-row.py
+++ 500-keyboard-row/500-keyboard-row.py
@@ -8,18 +8,18 @@ class Solution:
             for ch in row:
                 lines[ch] = i
         
-        for word in words:
-            _word = word.lower()
-            first = lines[_word[0]]
-            is_from_single_line = True
+#         for word in words:
+#             _word = word.lower()
+#             first = lines[_word[0]]
+#             is_from_single_line = True
             
-            for ch in _word:
-                if lines[ch] != first:
-                    is_from_single_line = False
-                    break
+#             for ch in _word:
+#                 if lines[ch] != first:
+#                     is_from_single_line = False
+#                     break
                     
-            if is_from_single_line:
-                res.append(word)
+#             if is_from_single_line:
+#                 res.append(word)
                 
-        return res
+        return [word for word in words if all(lines[ch] == lines[word.lower()[0]] for ch in word.lower())]
         
