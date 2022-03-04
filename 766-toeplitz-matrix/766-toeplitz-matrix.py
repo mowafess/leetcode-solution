3bd01cfa7dd042e460397c6bcb6210610f69fb5d
mowafess <36759376+mowafess@users.noreply.github.com>
2022-03-04 05:56:04 +0000

    Time: 170 ms (11.92%), Space: 13.9 MB (88.90%) - LeetHub

--- 766-toeplitz-matrix/766-toeplitz-matrix.py
+++ 766-toeplitz-matrix/766-toeplitz-matrix.py
@@ -1,30 +1,44 @@
 class Solution:
     def isToeplitzMatrix(self, matrix: List[List[int]]) -> bool:
-        R = len(matrix)
-        C = len(matrix[0])
         
-        def extractDiagonal(r, c):            
-            res = []
-            while 0 <= r < R and 0 <= c < C:
-                res.append(matrix[r][c])
-                r += 1
-                c += 1
-            return res
+        groups = {}
         
-        def isSameValue(first, arr):
-            return all(x == first for x in arr)
-        
-        c = 0
-        for r in range(R):
-            if not isSameValue(matrix[r][c], extractDiagonal(r, c)):
-                return False
-            
-        r = 0
-        for c in range(C):
-            if not isSameValue(matrix[r][c], extractDiagonal(r, c)):
-                return False
+        for r, row in enumerate(matrix):
+            for c, val in enumerate(row):
+                if r-c not in groups:
+                    groups[r-c] = val
+                elif groups[r-c] != val:
+                    return False
         
         return True
+        
+        
+        
+#         R = len(matrix)
+#         C = len(matrix[0])
+        
+#         def extractDiagonal(r, c):            
+#             res = []
+#             while 0 <= r < R and 0 <= c < C:
+#                 res.append(matrix[r][c])
+#                 r += 1
+#                 c += 1
+#             return res
+        
+#         def isSameValue(first, arr):
+#             return all(x == first for x in arr)
+        
+#         c = 0
+#         for r in range(R):
+#             if not isSameValue(matrix[r][c], extractDiagonal(r, c)):
+#                 return False
+            
+#         r = 0
+#         for c in range(C):
+#             if not isSameValue(matrix[r][c], extractDiagonal(r, c)):
+#                 return False
+        
+#         return True
         
             
                 
