982df1e0cc85a9fb239094a022f66d5ae8ab0308
mowafess <36759376+mowafess@users.noreply.github.com>
2022-03-08 22:19:52 +0000

    Time: 46 ms (77.51%), Space: 13.9 MB (77.85%) - LeetHub

--- 179-largest-number/179-largest-number.py
+++ 179-largest-number/179-largest-number.py
@@ -1,3 +1,5 @@
+from functools import cmp_to_key
+
 class Solution:
     def largestNumber(self, nums: List[int]) -> str:
         
@@ -5,11 +7,18 @@ class Solution:
             if x+y > y+x: return -1
             elif x+y < y+x: return 1
             else: return 0
-        
-        
-        from functools import cmp_to_key
+    
         output = ''.join(sorted(map(str, nums), key=cmp_to_key(comparator)))
         
         return '0' if output[0] == '0' else output
         
+
+# class LargerNumKey(str):
+#     def __lt__(x, y):
+#         return x+y > y+x
+        
+# class Solution:
+#     def largestNumber(self, nums):
+#         largest_num = ''.join(sorted(map(str, nums), key=LargerNumKey))
+#         return '0' if largest_num[0] == '0' else largest_num
         
