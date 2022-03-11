c52e1d1f250778616f548bb66ab45b1f26befe2d
mowafess <36759376+mowafess@users.noreply.github.com>
2022-03-11 08:49:52 +0000

    Time: 540 ms (99.15%), Space: 17.3 MB (91.56%) - LeetHub

--- 15-3sum/15-3sum.py
+++ 15-3sum/15-3sum.py
@@ -3,22 +3,22 @@ class Solution:
         
         nums.sort()
         output = set()       
-        def twoSum(nums, target, curr):
+        def twoSum(start, target, curr):
             seen = {}
             
-            for num in nums:
+            for num in nums[start:]:
                 if num in seen:
                     output.add((curr, seen[num], num))
                 seen[target - num] = num
                 
         
         for i in range(len(nums)):
-            # if nums[i] > 0:
-            #     break
+            if nums[i] > 0: # optional [for improvement]
+                break
             
-            # if i == 0 or nums[i - 1] != nums[i]:
-            target = 0 - nums[i]
-            twoSum(nums[i+1:], target, nums[i])
+            if i == 0 or nums[i - 1] != nums[i]: # optional [for improvement]
+                target = 0 - nums[i]
+                twoSum(i+1, target, nums[i])
                 
         return map(list, output)
             
