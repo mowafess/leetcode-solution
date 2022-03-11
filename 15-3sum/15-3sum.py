154f399658d454e9b0fc3a8e3582762eb2faaa63
mowafess <36759376+mowafess@users.noreply.github.com>
2022-03-11 08:47:35 +0000

    Time: 711 ms (93.47%), Space: 17.2 MB (92.69%) - LeetHub

---
+++ 15-3sum/15-3sum.py
@@ -0,0 +1,25 @@
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        
+        nums.sort()
+        output = set()       
+        def twoSum(nums, target, curr):
+            seen = {}
+            
+            for num in nums:
+                if num in seen:
+                    output.add((curr, seen[num], num))
+                seen[target - num] = num
+                
+        
+        for i in range(len(nums)):
+            # if nums[i] > 0:
+            #     break
+            
+            if i == 0 or nums[i - 1] != nums[i]:
+                target = 0 - nums[i]
+                twoSum(nums[i+1:], target, nums[i])
+                
+        return map(list, output)
+            
+        
