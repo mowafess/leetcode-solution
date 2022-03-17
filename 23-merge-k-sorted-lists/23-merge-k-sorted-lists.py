e4a5843f4507a9e316f5de98b143cc3d593fadeb
Mowaninuola Osifeso <36759376+mowafess@users.noreply.github.com>
2022-03-17 21:54:26 +0000

    Time: 5203 ms (5.30%), Space: 17.7 MB (79.88%) - LeetHub

--- 23-merge-k-sorted-lists/23-merge-k-sorted-lists.py
+++ 23-merge-k-sorted-lists/23-merge-k-sorted-lists.py
@@ -3,20 +3,20 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-class Solution:
+# class Solution:
     
-    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-        # T - O(NlogN); S - O(N)
-        self.nodes = []
-        head = point = ListNode(0)
-        for l in lists:
-            while l:
-                self.nodes.append(l.val)
-                l = l.next
-        for x in sorted(self.nodes):
-            point.next = ListNode(x)
-            point = point.next
-        return head.next
+#     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+#         # T - O(NlogN); S - O(N)
+#         self.nodes = []
+#         head = point = ListNode(0)
+#         for l in lists:
+#             while l:
+#                 self.nodes.append(l.val)
+#                 l = l.next
+#         for x in sorted(self.nodes):
+#             point.next = ListNode(x)
+#             point = point.next
+#         return head.next
 
 #     def mergeList(self, first, second):
 #         prev = res = ListNode(float("-inf"))
@@ -44,3 +44,30 @@ class Solution:
 #             return self.mergeList(self.mergeKLists(lists[:len(lists) // 2]), \
 #                                       self.mergeKLists(lists[len(lists) // 2: ]))
         
+    
+class Solution:
+    
+    def mergeList(self, first, second):
+        prev = res = ListNode(float("-inf"))
+        
+        while first and second:
+            if first.val < second.val:
+                prev.next = first
+                first = first.next
+            else:
+                prev.next = second
+                second = second.next
+            
+            prev = prev.next
+        
+        prev.next = first if first else second
+        
+        return res.next
+        
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        if lists:
+            first = lists[0]
+            for i in range(1, len(lists)):
+                first = self.mergeList(first, lists[i])
+            
+            return first
