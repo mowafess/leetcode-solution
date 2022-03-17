7230a7ff068d3430de2c183fee953223105c7750
Mowaninuola Osifeso <36759376+mowafess@users.noreply.github.com>
2022-03-17 21:50:27 +0000

    Time: 88 ms (99.52%), Space: 18.5 MB (15.84%) - LeetHub

---
+++ 23-merge-k-sorted-lists/23-merge-k-sorted-lists.py
@@ -0,0 +1,46 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        # T - O(NlogN); S - O(N)
+        self.nodes = []
+        head = point = ListNode(0)
+        for l in lists:
+            while l:
+                self.nodes.append(l.val)
+                l = l.next
+        for x in sorted(self.nodes):
+            point.next = ListNode(x)
+            point = point.next
+        return head.next
+
+#     def mergeList(self, first, second):
+#         prev = res = ListNode(float("-inf"))
+        
+#         while first and second:
+#             if first.val < second.val:
+#                 prev.next = first
+#                 first = first.next
+#             else:
+#                 prev.next = second
+#                 second = second.next
+            
+#             prev = prev.next
+        
+#         prev.next = first if first else second
+        
+#         return res.next
+        
+#     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+#         if(len(lists)==0):
+#             return ListNode().next
+#         elif(len(lists)==1):
+#             return lists[0]
+#         else:
+#             return self.mergeList(self.mergeKLists(lists[:len(lists) // 2]), \
+#                                       self.mergeKLists(lists[len(lists) // 2: ]))
+        
