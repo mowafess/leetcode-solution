0c5a086c99e2b9b2fb813efeb8667db9d9b18ff3
Mowaninuola Osifeso <36759376+mowafess@users.noreply.github.com>
2022-03-17 20:00:06 +0000

    Time: 1315 ms (5.06%), Space: 22.9 MB (20.51%) - LeetHub

--- 1985-find-the-kth-largest-integer-in-the-array/1985-find-the-kth-largest-integer-in-the-array.py
+++ 1985-find-the-kth-largest-integer-in-the-array/1985-find-the-kth-largest-integer-in-the-array.py
@@ -2,8 +2,8 @@ class Element(str):
     def __lt__(self, other):
         return int(self) < int(other)
     
-    def __gt__(self, other):
-        return int(self) > int(other)
+    # def __gt__(self, other):
+    #     return int(self) > int(other)
 
 class Solution:
     def kthLargestNumber(self, nums: List[str], k: int) -> str:
@@ -12,17 +12,27 @@ class Solution:
         # extract first k elements
         # heapify
         
-        nums = [*map(lambda num: Element(num), nums)]
-        heap = nums[:k] 
-        
-        # return heapq.nlargest(k, nums)[-1]
-        
+        heap = []
         heapq.heapify(heap)
         
-        for num in nums[k:]:
-            if num > heap[0]:
+        for num in nums:
+            heapq.heappush(heap, Element(num))
+            if len(heap) > k:
                 heapq.heappop(heap)
-                heapq.heappush(heap, num)
         
-        return heapq.heappop(heap)
+        return heap[0]
+        
+#         nums = [*map(lambda num: Element(num), nums)]
+#         heap = nums[:k] 
+        
+#         # return heapq.nlargest(k, nums)[-1]
+        
+#         heapq.heapify(heap)
+        
+#         for num in nums[k:]:
+#             if num > heap[0]:
+#                 heapq.heappop(heap)
+#                 heapq.heappush(heap, num)
+        
+#         return heapq.heappop(heap)
         
