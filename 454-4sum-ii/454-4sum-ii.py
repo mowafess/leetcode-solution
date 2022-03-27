98dfeccb367ad94241ba794a28c71a8d715fcf68
Mowaninuola Osifeso <36759376+mowafess@users.noreply.github.com>
2022-03-27 18:39:31 +0100

    Time: 593 ms (93.64%), Space: 14.2 MB (34.40%) - LeetHub

--- 454-4sum-ii/454-4sum-ii.py
+++ 454-4sum-ii/454-4sum-ii.py
@@ -1,22 +1,23 @@
-# class Solution:
-#     def fourSumCount(self, nums1: List[int], nums2: List[int], nums3: List[int], nums4: List[int]) -> int:
-from collections import Counter
-from itertools import product
 class Solution:
-    def fourSumCount(self, *lists):
-        k = len(lists)
-        sum_to_count = Counter(sum(nums) for nums in product(*lists[:k // 2]))
-        return sum(sum_to_count[-sum(nums)] for nums in product(*lists[k // 2:]))
-        d = {}
+    def fourSumCount(self, nums1: List[int], nums2: List[int], nums3: List[int], nums4: List[int]) -> int:
+
+        d = defaultdict(int)
         ans = 0
         
         for i in nums1:
             for j in nums2:
-                d[(i+j)] = d.get((i+j), 0) + 1
+                d[i+j] += 1
         
         for i in nums3:
             for j in nums4:
-                ans += d.get(-(i+j), 0)
+                ans += d[-(i+j)]
         
         return ans
+    
+# class Solution:
+#     def fourSumCount(self, *lists):
+#         k = len(lists)
+#         sum_to_count = Counter(sum(nums) for nums in product(*lists[:k // 2]))
+#         return sum(sum_to_count[-sum(nums)] for nums in product(*lists[k // 2:]))
+
     
