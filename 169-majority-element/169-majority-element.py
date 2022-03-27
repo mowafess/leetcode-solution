6230de83cf2f76317d3f7e15c66d20c3813917f5
Mowaninuola Osifeso <36759376+mowafess@users.noreply.github.com>
2022-03-27 11:49:38 +0100

    Time: 164 ms (97.04%), Space: 15.5 MB (29.17%) - LeetHub

--- 169-majority-element/169-majority-element.py
+++ 169-majority-element/169-majority-element.py
@@ -1,14 +1,22 @@
-
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        current_element = nums[0]
-        count = 1
-        for i in range(1, len(nums)):
-            if nums[i] != current_element:
-                count -=1
-                if count == 0:
-                    current_element = nums[i]
-                    count = 1
+        majority = nums[0]
+        votes = 0
+        
+        for num in nums:
+            if votes == 0:
+                majority = num
+                votes += 1
+            elif majority != num:
+                votes -= 1
             else:
-                count+=1
-        return current_element 
+                votes += 1
+        
+        return majority
+                
+        
+        
+        
+        # T-O(N); S-O(N)
+        # return collections.Counter(nums).most_common(1)[-1][0]
+        
