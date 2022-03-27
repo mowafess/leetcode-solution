b1e660356ec3999142f8ee619b31aba3ee8c6f9d
Mowaninuola Osifeso <36759376+mowafess@users.noreply.github.com>
2022-03-27 17:53:42 +0100

    Time: 121 ms (68.25%), Space: 15.7 MB (66.69%) - LeetHub

--- 207-course-schedule/207-course-schedule.py
+++ 207-course-schedule/207-course-schedule.py
@@ -1,32 +1,54 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        
+        total = 0
+        graph = defaultdict(set)
+        in_degree = defaultdict(int)
+        
+        for course, prereq in prerequisites:
+            graph[prereq].add(course)
+            in_degree[course] += 1
+        
+        queue = deque([c for c in range(numCourses) if c not in in_degree])
+        
+        while queue:
+            node = queue.popleft()
+            total += 1
+            
+            for nei in graph[node]:
+                in_degree[nei] -= 1
+                if in_degree[nei] == 0:
+                    queue.append(nei)
+        
+        return numCourses == total
+        
         # find cicular ref
         
-        graph = collections.defaultdict(set)
+#         graph = collections.defaultdict(set)
         
-        for course, prerequisite in prerequisites:
-            graph[course].add(prerequisite)
+#         for course, prerequisite in prerequisites:
+#             graph[course].add(prerequisite)
            
-        def dfs(course):
-            if visited[course]:
-                return False
-            if not graph[course]:
-                return True
+#         def dfs(course):
+#             if visited[course]:
+#                 return False
+#             if not graph[course]:
+#                 return True
             
-            visited[course] = 1
-            result = True
+#             visited[course] = 1
+#             result = True
             
-            for prereq in graph[course]:
-                if not dfs(prereq):
-                    return False
+#             for prereq in graph[course]:
+#                 if not dfs(prereq):
+#                     return False
                 
-            visited[course] = 0
-            graph[course] = set()
+#             visited[course] = 0
+#             graph[course] = set()
             
-            return True
+#             return True
             
-        visited = [0] * numCourses
+#         visited = [0] * numCourses
         
-        for course in range(numCourses):
-            if not dfs(course): return False
-        return True
+#         for course in range(numCourses):
+#             if not dfs(course): return False
+#         return True
