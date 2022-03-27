bee4c0548a80d3e8e18c0d0bb8324c18fba61959
Mowaninuola Osifeso <36759376+mowafess@users.noreply.github.com>
2022-03-27 23:23:21 +0100

    Time: 32 ms (86.03%), Space: 14 MB (22.75%) - LeetHub

---
+++ 738-monotone-increasing-digits/738-monotone-increasing-digits.py
@@ -0,0 +1,15 @@
+class Solution:
+    def monotoneIncreasingDigits(self, n: int) -> int:
+        if n < 10:
+            return n
+        
+        s = str(n)
+        
+        for i in range(len(s)-1, 0, -1):
+            print(i, n, s)
+            if s[i] < s[i-1]:
+                n -= int(s[i:]) + 1
+                s = str(n)
+        
+        return int(s)
+        
