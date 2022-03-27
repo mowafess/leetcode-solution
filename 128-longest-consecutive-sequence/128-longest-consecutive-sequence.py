490979bfc61ee3f603240291101a8221ed337ea1
Mowaninuola Osifeso <36759376+mowafess@users.noreply.github.com>
2022-03-27 16:02:09 +0100

    Time: 3403 ms (9.48%), Space: 27.8 MB (34.68%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.py
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.py
@@ -4,19 +4,20 @@ class Solution:
         # T - O(NlogN)
         
         num_set = set(nums)
-        seen = set()
+        seen = set() # to avoid a repeat work
         longest = 0
         
         for n in nums:
-            if n in seen:
-                continue
+            # if n in seen:
+            #     continue
+                
             # check if its the start of a sequence?
             if n - 1 not in num_set:
-                seen.add(n)
+                # seen.add(n)
                 curr = 1
                 while n + curr in num_set:
                     curr += 1
-                    seen.add(n + curr)
+                    # seen.add(n + curr)
                 longest  = max(longest , curr)
                 
         return longest 
