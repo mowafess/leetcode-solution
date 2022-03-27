214703f2514c9d5791b8df27354d05aaf4962dcb
Mowaninuola Osifeso <36759376+mowafess@users.noreply.github.com>
2022-03-27 17:37:33 +0100

    Time: 107 ms (84.00%), Space: 15.6 MB (67.88%) - LeetHub

--- 210-course-schedule-ii/210-course-schedule-ii.py
+++ 210-course-schedule-ii/210-course-schedule-ii.py
@@ -30,39 +30,25 @@ class Solution:
         
         
     def findOrder(self, numCourses, prerequisites):
-        """
-        :type numCourses: int
-        :type prerequisites: List[List[int]]
-        :rtype: List[int]
-        """
 
-        # Prepare the graph
-        adj_list = defaultdict(list)
+        graph = defaultdict(list)
         indegree = {}
         for dest, src in prerequisites:
-            adj_list[src].append(dest)
-
-            # Record each node's in-degree
+            graph[src].append(dest)
             indegree[dest] = indegree.get(dest, 0) + 1
 
-        # Queue for maintainig list of nodes that have 0 in-degree
         zero_indegree_queue = deque([k for k in range(numCourses) if k not in indegree])
 
         topological_sorted_order = []
 
-        # Until there are nodes in the Q
         while zero_indegree_queue:
+            
+            node = zero_indegree_queue.popleft()
+            topological_sorted_order.append(node)
 
-            # Pop one node with 0 in-degree
-            vertex = zero_indegree_queue.popleft()
-            topological_sorted_order.append(vertex)
-
-            # Reduce in-degree for all the neighbors
-            if vertex in adj_list:
-                for neighbor in adj_list[vertex]:
+            if node in graph:
+                for neighbor in graph[node]:
                     indegree[neighbor] -= 1
-
-                    # Add neighbor to Q if in-degree becomes 0
                     if indegree[neighbor] == 0:
                         zero_indegree_queue.append(neighbor)
 
