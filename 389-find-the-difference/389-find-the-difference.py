bf0fc03433ad6b92842f251099258a3ba646b870
Mowaninuola Osifeso <36759376+mowafess@users.noreply.github.com>
2022-03-27 12:00:52 +0100

    Time: 50 ms (54.09%), Space: 13.9 MB (79.14%) - LeetHub

---
+++ 389-find-the-difference/389-find-the-difference.py
@@ -0,0 +1,12 @@
+class Solution:
+    def findTheDifference(self, s: str, t: str) -> str:
+        s_count = Counter(s)
+        t_count = Counter(t)
+        
+        for ch in t:
+            if ch not in s_count or s_count[ch] != t_count[ch]:
+                return ch
+            
+        return ''
+        
+        
