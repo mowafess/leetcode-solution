eb4e40b654cf85f4129e5ab023cc34f15b1dd770
Mowaninuola Osifeso <36759376+mowafess@users.noreply.github.com>
2023-06-04 07:23:55 +0100

    Time: 98 ms (80.53%), Space: 14.4 MB (46.90%) - LeetHub

--- 0849-maximize-distance-to-closest-person/0849-maximize-distance-to-closest-person.py
+++ 0849-maximize-distance-to-closest-person/0849-maximize-distance-to-closest-person.py
@@ -4,8 +4,24 @@ class Solution(object):
         :type seats: List[int]
         :rtype: int
         """
+        
+        first_index = seats.index(1)
+        last_index = seats[::-1].index(1)
+        
+        max_dist = max(first_index, last_index)
+        
+        for i in range(first_index + 1, len(seats)):
+            if seats[i]:
+                dist = (i - first_index) // 2
+                max_dist = max(max_dist, dist)
+                first_index = i
+        
+        return max_dist
+        
+        
         output = [0] * len(seats)
-        prev = -len(seats)
+        prev = -len(seats) #or first index of 1
+        
         
         for i in range(len(seats)):
             if not seats[i]:
