3392daab8f2fea43ef1355a90cb3baad6045639b
Mowaninuola Osifeso <36759376+mowafess@users.noreply.github.com>
2023-07-17 23:31:39 +0100

    Time: 93 ms (34.32%), Space: 19.5 MB (25.24%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -5,9 +5,23 @@ class Solution(object):
         :rtype: List[List[str]]
         """
         
+#         groups = collections.defaultdict(list)
+        
+#         for word in strs:
+#             groups[tuple(sorted(word))].append(word)
+        
+#         return list(groups.values())
+
+        def encode(word):
+            char_counter = [0] * 26
+            for ch in word:
+                index = ord(ch) - ord('a')
+                char_counter[index] += 1
+            return tuple(char_counter)
+        
         groups = collections.defaultdict(list)
         
         for word in strs:
-            groups[tuple(sorted(word))].append(word)
+            groups[encode(word)].append(word)
         
         return list(groups.values())
