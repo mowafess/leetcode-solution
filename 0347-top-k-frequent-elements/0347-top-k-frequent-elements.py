3d255a2aa79b6c51a1ffc69dce5a37bbd51bba3d
Mowaninuola Osifeso <36759376+mowafess@users.noreply.github.com>
2023-07-18 00:10:33 +0100

    Time: 96 ms (34.79%), Space: 20.9 MB (22.39%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -6,6 +6,29 @@ class Solution(object):
         :rtype: List[int]
         """
         
+        
         from collections import Counter
         
-        return [num for num, val in Counter(nums).most_common(k)]
+        
+        # this rely on heap
+        # T - O(NlogK)
+        # S - O(N)
+        # return [num for num, val in Counter(nums).most_common(k)]
+        
+        # another approach
+        # T - O(N)
+        # group then based on their freq
+        freq = Counter(nums)
+        buckets = [[] for i in range(len(nums) + 1)]
+        
+        for key, val in freq.items():
+            buckets[val].append(key)
+        
+        res = []
+        
+        for i in range(len(buckets)-1, 0, -1):
+            for num in buckets[i]:
+                res.append(num)
+                if k == len(res):
+                    return res
+        
