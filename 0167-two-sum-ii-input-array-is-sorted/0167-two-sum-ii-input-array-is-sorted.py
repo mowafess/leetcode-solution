3f837456fa0be1eabbdc7986c94b443f550f9a76
Mowaninuola Osifeso <36759376+mowafess@users.noreply.github.com>
2023-07-18 21:21:31 +0100

    Time: 103 ms (62.21%), Space: 14.2 MB (32.30%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
@@ -5,13 +5,32 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
-        mapping = {}
+#         mapping = {}
         
-        for idx, num in enumerate(numbers, start=1):
-            complement = target - num
-            if complement in mapping:
-                return [mapping[complement], idx]
-            mapping[num] = idx
+#         for idx, num in enumerate(numbers, start=1):
+#             complement = target - num
+#             if complement in mapping:
+#                 return [mapping[complement], idx]
+#             mapping[num] = idx
         
+#         return [-1, -1]
+
+
+        l, r = 0, len(numbers) - 1
+        
+        while l < r:
+            curr_sum = numbers[l] + numbers[r]
+            
+            if curr_sum == target:
+                return [l+1, r+1]
+            
+            if curr_sum > target:
+                r -= 1
+            else:
+                l += 1
+                
         return [-1, -1]
+                
+                
+            
         
