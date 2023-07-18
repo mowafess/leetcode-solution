226865277bbece7c4e1bd77528a1c76a274f865c
Mowaninuola Osifeso <36759376+mowafess@users.noreply.github.com>
2023-07-18 21:11:03 +0100

    Time: 921 ms (67.61%), Space: 19.5 MB (5.05%) - LeetHub

---
+++ 0015-3sum/0015-3sum.py
@@ -0,0 +1,33 @@
+class Solution(object):
+    def threeSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        TOTAL = 0
+        
+        nums.sort()
+        
+        output = set()
+        
+        def twoSum(arr, target, curr):
+            seen = set()
+            
+            for num in arr:
+                comp = target - num
+                if comp in seen:
+                    output.add((curr, comp, num))
+                seen.add(num)
+            
+             
+        for idx, num in enumerate(nums):
+            if num > TOTAL:
+                break
+                
+            if idx > 0 and nums[idx - 1] == num:
+                continue
+            target = TOTAL - num
+            twoSum(nums[idx + 1:], target, num)
+            
+        
+        return map(list, output)
