4544e82858d6861db878bcae824b63658b22cae0
Mowaninuola Osifeso <36759376+mowafess@users.noreply.github.com>
2023-07-18 21:43:40 +0100

    Time: 740 ms (89.47%), Space: 16.7 MB (74.06%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -4,27 +4,67 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[List[int]]
         """
-        TOTAL = 0
-        nums.sort()
-        output = set()
+#         TOTAL = 0
+#         nums.sort()
+#         output = set()
         
-        def twoSum(arr, target, curr):
-            seen = set()
+# #         def twoSum(arr, target, curr):
+# #             seen = set()
             
-            for num in arr:
-                comp = target - num
-                if comp in seen:
-                    output.add((curr, comp, num))
-                seen.add(num)
+# #             for num in arr:
+# #                 comp = target - num
+# #                 if comp in seen:
+# #                     output.add((curr, comp, num))
+# #                 seen.add(num)
             
-             
+        
+#         for idx, num in enumerate(nums):
+#             if num > TOTAL:
+#                 break
+                
+#             if idx == 0 or nums[idx - 1] != num:
+#                 target = TOTAL - num
+#                 twoSum(nums[idx + 1:], target, num)
+            
+        
+#         return map(list, output)
+
+
+# ---
+        TOTAL = 0
+
+        res = []
+        nums.sort()
+        
         for idx, num in enumerate(nums):
             if num > TOTAL:
                 break
-                
-            if idx == 0 or nums[idx - 1] != num:
-                target = TOTAL - num
-                twoSum(nums[idx + 1:], target, num)
             
+            if idx == 0 or nums[idx-1] != num:
+                l, r = idx + 1, len(nums) - 1
+                
+                while l < r:
+                    total = num + nums[l] + nums[r]
+                    
+                    if total == TOTAL:
+                        res.append([num, nums[l], nums[r]])
+                        
+                        # we only need to move one of the pointers in the case
+                        # the other if blocks would take care of the movement of
+                        # of the other
+                        
+                        l += 1
+                        while nums[l] == nums[l - 1] and l < r:
+                            l += 1
+                    
+                    elif total > TOTAL:
+                        r -= 1
+                    
+                    else:
+                        l += 1
+                
         
-        return map(list, output)
+        return res
+                    
+                        
+                        
