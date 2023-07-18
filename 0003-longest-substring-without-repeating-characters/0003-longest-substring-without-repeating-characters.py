7bdcbee91a1d4c8741d4bd8aeccca3a05a045b1e
Mowaninuola Osifeso <36759376+mowafess@users.noreply.github.com>
2023-07-18 23:24:43 +0100

    Time: 48 ms (57.10%), Space: 14.3 MB (19.72%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -5,17 +5,35 @@ class Solution(object):
         :rtype: int
         """
         
+        cache = set()
+        l = 0
+        longest = 0
+        
+        for r in range(len(s)):
+            while s[r] in cache:
+                cache.remove(s[l])
+                l += 1
+            
+            cache.add(s[r])
+            
+            longest = max(longest, r - l + 1)
+            
+        return longest
+        
         cache = collections.defaultdict(int)
         
         l, r, longest = 0, 0, 0
         
         while r < len(s):
+            # shrink window if new item (r) already exist
             while cache[s[r]] and l < r:
                 cache[s[l]] -= 1
                 l += 1
             
+            # else expand window
             cache[s[r]] += 1
             r += 1
+            
             longest = max(longest, r - l)
             
         
