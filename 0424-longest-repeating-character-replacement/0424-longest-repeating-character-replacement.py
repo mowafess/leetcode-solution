db3d4c66442992c64efb5be679ae9c183e474775
Mowaninuola Osifeso <36759376+mowafess@users.noreply.github.com>
2023-07-19 01:08:18 +0100

    Time: 123 ms (40.30%), Space: 13.9 MB (27.65%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -5,13 +5,29 @@ class Solution(object):
         :type k: int
         :rtype: int
         """
-        l, longest = 0, 0
+#         l, longest = 0, 0
+#         cache = collections.Counter()
+        
+#         for r in range(len(s)):
+#             cache[s[r]] += 1
+            
+#             while (r - l + 1) - cache.most_common(1)[0][1] > k:
+#                 cache[s[l]] -= 1
+#                 l += 1
+            
+#             longest = max(longest, r - l + 1)
+            
+        
+#         return longest
+    
+        l, longest, max_freq = 0, 0, 0
         cache = collections.Counter()
         
         for r in range(len(s)):
             cache[s[r]] += 1
+            max_freq = max(max_freq, cache[s[r]])
             
-            while (r - l + 1) - cache.most_common(1)[0][1] > k:
+            while (r - l + 1) - max_freq > k:
                 cache[s[l]] -= 1
                 l += 1
             
