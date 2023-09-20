f5461ed8c3b5765dcd506035601cc953b7114758
Mowaninuola Osifeso <36759376+mowafess@users.noreply.github.com>
2023-09-20 20:21:16 +0100

    Time: 76 ms (46.55%), Space: 17.6 MB (31.39%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,17 +1,22 @@
-class Solution(object):
-    def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+
+        # double for loop approach
+        # Time: O(N^2); Space: O(1)
+        # for i in range(len(nums)):
+        #     for j in range(i+1, len(nums)):
+        #         if nums[i] + nums[j] == target:
+        #             return i, j
+
         
-        seen = {}
-        
+        # optimize with dict as cache
+        #  Time: O(N); Space: O(N)
+        cache = {}
+
         for idx, num in enumerate(nums):
             comp = target - num
-            if comp in seen:
-                return [seen[comp], idx]
-            seen[num] = idx
-        
-        return [-1, -1]
+            if comp in cache:
+                return cache[comp], idx
+            cache[num] = idx
+
+        return -1, -1        
