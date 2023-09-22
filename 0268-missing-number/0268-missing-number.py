97919a8598f45e25f374c15d402f3e960aabc654
Mowaninuola Osifeso <36759376+mowafess@users.noreply.github.com>
2023-09-22 11:59:37 +0100

    Time: 135 ms (31.15%), Space: 17.8 MB (17.83%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -5,7 +5,7 @@ def binary_search(arr: List[int]) -> int:
     while lo < hi:
         mid = (lo + hi) // 2
         if mid < arr[mid]:
-            hi = mid - 1
+            hi = mid
         else:
             lo = mid + 1
         
@@ -14,7 +14,7 @@ def binary_search(arr: List[int]) -> int:
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
         
-        # return binary_search(sorted(nums))
+        return binary_search(sorted(nums))
         
         # if sorted use binary search
         
@@ -24,9 +24,9 @@ class Solution:
         
 #         return ((n + 1) * n ) // 2 - total
 
-        missing = len(nums)
+#         missing = len(nums)
     
-        for i, num in enumerate(nums):
-            missing ^= num ^ i
+#         for i, num in enumerate(nums):
+#             missing ^= num ^ i
             
-        return missing
+#         return missing
