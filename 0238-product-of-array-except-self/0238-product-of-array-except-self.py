fdd26c4f7709774d6cad06eeb28d6e67456470af
Mowaninuola Osifeso <36759376+mowafess@users.noreply.github.com>
2024-02-25 20:00:19 +0000

    Time: 162 ms (78.31%), Space: 23.8 MB (85.12%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -1,48 +1,15 @@
-from operator import mul
-
-class Solution(object):
-    def productExceptSelf(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: List[int]
-        """
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
         
-        # brute force - TLE | time - O(N^2)
+        n = len(nums)
+        cache = [1] * n
         
-#         output = []
-        
-#         for i in range(len(nums)):
-#             before = nums[:i]
-#             after = nums[i+1:]
-            
-#             output.append(reduce(mul, before + after))
-        
-        # return output
-    
-        # how can we precompute the before and after?
-        # what data structure do we use to cache the result
-        # do we use one cache or two?
-        
-        # [1,2,3,4]
-        
-        # [1, 1, 2, 6] - before
-        # [24, 12, 4, 1] - after
-        
-        # ---
-        
-        cache = [1] * len(nums)
-        
-        for i in range(len(nums) - 1):
-            cache[i + 1] = cache[i] * nums[i]
+        for i in range(1, n):
+            cache[i] = cache[i - 1] * nums[i - 1]
         
         prev = 1
-        for i in reversed(range(len(nums) - 1)):
+        for i in reversed(range(n - 1)):
             prev *= nums[i + 1]
             cache[i] *= prev
-        
+            
         return cache
-        
-        
-        
-        
-        
