7913c1b91531e3e524d9822f03ffddc3ed3a026f
Mowaninuola Osifeso <36759376+mowafess@users.noreply.github.com>
2025-02-20 13:08:26 +0000

    Update 0015-3sum.py

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -24,3 +24,39 @@ class Solution:
             twoSums(idx+1, num, 0 - num)
                 
         return map(list, output)
+
+# avoiding duplicates
+class Solution2:
+    def threeSum(self, nums: list[int]):
+        nums.sort()
+        out = []
+        r = len(nums) - 1
+
+        def twoSum(curr, l, r):
+            while l < r:
+                total = curr + nums[l] + nums[r]
+                if total == 0:
+                    out.append((curr, nums[l], nums[r]))
+                    while l < r and nums[l] == nums[l + 1]:
+                        l += 1
+                    while l < r and nums[r] == nums[r - 1]:
+                        r -= 1
+                    l += 1
+                    r -= 1
+                elif total > 0:
+                    r -= 1
+                else:
+                    l += 1
+    
+
+
+        prev = float("-inf")
+        for i, num in enumerate(nums):
+            if num == prev:
+                continue
+            
+            twoSum(num, i + 1, r)
+
+            prev = num
+        
+        return out
