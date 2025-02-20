e921ef1f5c27747f528e1337a56678239f93dea0
Mowaninuola Osifeso <36759376+mowafess@users.noreply.github.com>
2025-02-20 14:43:38 +0000

    Update variable-length.py

--- hello/01-two-pointer/variable-length.py
+++ hello/01-two-pointer/variable-length.py
@@ -18,3 +18,24 @@ def fruit_into_baskets(fruits):
     max_fruit = max(max_fruit, end - start + 1)
 
   return max_fruit
+
+
+# template
+def variable_length_sliding_window(nums):
+  state = # choose appropriate data structure
+  start = 0
+  max_ = 0
+
+  for end in range(len(nums)):
+    # extend window
+    # add nums[end] to state in O(1) in time
+
+    while state is not valid:
+      # repeatedly contract window until it is valid again
+      # remove nums[start] from state in O(1) in time
+      start += 1
+
+    # INVARIANT: state of current window is valid here.
+    max_ = max(max_, end - start + 1)
+
+  return max_
