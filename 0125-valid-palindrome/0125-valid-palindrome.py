04565956782949c80e3afce8dfa0a16719dfba35
Mowa O <36759376+mowafess@users.noreply.github.com>
2025-06-05 06:12:52 +0100

    Time: 8 ms (52.94%), Space: 17.9 MB (95.22%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,21 +1,41 @@
-class Solution(object):
-    def isPalindrome(self, s):
-        """
-        :type s: str
-        :rtype: bool
-        """
-        s = s.lower()
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+
         l, r = 0, len(s) - 1
-        
+
         while l < r:
-            if not s[l].isalnum():
+            while l < r and not s[l].isalnum():
                 l += 1
-            elif not s[r].isalnum():
+            while l < r and not s[r].isalnum():
                 r -= 1
-            elif s[l] != s[r]:
+            
+            if s[l].lower() != s[r].lower():
                 return False
-            else:
-                l += 1
-                r -= 1
+
+            l += 1
+            r -= 1
         
         return True
+        
+        # left = 0
+        # right = len(s) - 1
+        
+        # s = s.lower()
+        
+        
+        # while left < right:
+        #     if not s[left].isalnum():
+        #         left += 1
+        #     elif not s[right].isalnum():
+        #         right -= 1
+        #     elif s[right] != s[left]:
+        #         return False
+        #     else:
+        #         left += 1
+        #         right -= 1
+            
+        
+        # return True
+            
+            
+        
