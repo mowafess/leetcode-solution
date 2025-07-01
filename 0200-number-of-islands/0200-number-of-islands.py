d492e3cfad2feed0006be0788977bca54de81a8c
Mowa O <36759376+mowafess@users.noreply.github.com>
2025-07-01 10:00:14 +0100

    Time: 254 ms (47.64%), Space: 20.4 MB (42.91%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,52 +1,33 @@
 class Solution:
-    pos = {(0, -1), (0, 1), (1, 0), (-1, 0)}
-
     def numIslands(self, grid: List[List[str]]) -> int:
 
+        if not grid or not grid[0]:
+            return 0
 
-                # bfs
-        queue = deque([])
-        xlim = len(grid)
-        ylim = len(grid[0])
-        count = 0
+        pos = {(-1, 0), (0, 1), (0, -1), (1, 0)}
+
+        total = 0
+        rows = len(grid)
+        cols = len(grid[0])
+
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == '1':
+                    grid[r][c] = '0'
+                    self.bfs(grid, r, c, pos, rows, cols)
+                    total += 1
+
+        return total
         
-        for x in range(xlim):
-            for y in range(ylim):
-                if grid[x][y] == '1':
-                    grid[x][y] = '0'
-                    queue.append((x, y))
-                    self.bfs(grid, queue, xlim, ylim)
-                    count += 1
+    def bfs(self, grid, r, c, pos, rows, cols):
+        is_within = lambda x, limit: 0 <= x < limit
 
-        return count
+        q = collections.deque([(r, c)])
 
-    def bfs(self, grid: List[List[str]], queue: Deque[tuple[int]], xlim: int, ylim: int):
-        isWithinbound = lambda grid, x, y, xlim, ylim: \
-            0 <= x < xlim and 0 <= y < ylim and grid[x][y] == '1'
-        while queue:
-            x, y = queue.popleft()
-            for del_x, del_y in self.pos:
-                new_x, new_y = x + del_x, y + del_y
-                if isWithinbound(grid, new_x, new_y, xlim, ylim):
-                    queue.append((new_x, new_y))
-                    grid[new_x][new_y] = '0'
-
-        # def dfs(r, c):
-        #     if grid[r][c] == "0":
-        #         return 
-        #     grid[r][c] = "0"
-
-        #     for dr, dc in dirs:
-        #         nr = r + dr
-        #         nc = c + dc
-        #         if 0 <= nr < rows and 0 <= nc < cols and grid[nr][nc] == "1":
-        #             dfs(nr, nc)
-
-        # for r in range(rows):
-        #     for c in range(cols):
-        #         if grid[r][c] == "1":
-        #             dfs(r, c)
-        #             total += 1
-
-        # return total
-        
+        while q:
+            r, c = q.popleft()
+            for dr, dc in pos:
+                nr, nc = r + dr, c + dc
+                if is_within(nr, rows) and is_within(nc, cols) and grid[nr][nc] == '1':
+                    q.append((nr, nc))
+                    grid[nr][nc] = '0'
