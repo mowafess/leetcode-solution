25cdff384d922ef1c7abe692e5e602cdeb0f75e9
Mowa O <36759376+mowafess@users.noreply.github.com>
2025-07-01 12:30:27 +0100

    Time: 8 ms (34.72%), Space: 24.2 MB (14%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -3,6 +3,8 @@ from collections import Counter
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
 
+        # return [i for i, v in Counter(nums).most_common(k)]
+
         freq = Counter(nums)
 
         # bucket sort make it O(n) - use when n is not so large
