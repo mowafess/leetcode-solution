3e5bb34c6ef9adde50a9296cb3bfb70ff5c3b535
Mowa O <36759376+mowafess@users.noreply.github.com>
2025-07-01 12:28:54 +0100

    Time: 15 ms (11.32%), Space: 24.6 MB (7.96%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,34 +1,21 @@
-class Solution(object):
-    def topKFrequent(self, nums, k):
-        """
-        :type nums: List[int]
-        :type k: int
-        :rtype: List[int]
-        """
-        
-        
-        from collections import Counter
-        
-        
-        # this rely on heap
-        # T - O(NlogK)
-        # S - O(N)
-        # return [num for num, val in Counter(nums).most_common(k)]
-        
-        # another approach
-        # T - O(N)
-        # group then based on their freq
+from collections import Counter
+
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+
         freq = Counter(nums)
-        buckets = [[] for i in range(len(nums) + 1)]
-        
-        for key, val in freq.items():
-            buckets[val].append(key)
+
+        # bucket sort make it O(n) - use when n is not so large
+        order = [[] for _ in range(len(nums) + 1)]
+    
+        for i, v in freq.items():
+            order[v].append(i)
         
         res = []
-        
-        for i in range(len(buckets)-1, 0, -1):
-            for num in buckets[i]:
+        for i in range(len(order) - 1, 0, -1):
+            for num in order[i]:
                 res.append(num)
-                if k == len(res):
+                if len(res) == k:
                     return res
+            
         
