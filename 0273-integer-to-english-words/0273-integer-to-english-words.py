61c360dc3ba45a67bd18f1f3762c8536ff4ae601
Mowa O <36759376+mowafess@users.noreply.github.com>
2025-07-06 21:38:20 +0100

    Time: 0 ms (100%), Space: 18.1 MB (25.55%) - LeetHub

--- 0273-integer-to-english-words/0273-integer-to-english-words.py
+++ 0273-integer-to-english-words/0273-integer-to-english-words.py
@@ -1,44 +1,47 @@
 class Solution:
     def numberToWords(self, num: int) -> str:
-
+        if num == 0:
+            return "Zero"
+        
         lt20 = [
-            'Zero', 'One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine',
-            'Ten', 'Eleven', 'Twelve', 'Thirteen', 'Fourteen', 'Fifteen', 'Sixteen',
+            '', 'One', 'Two', 'Three', 'Four', 'Five', 'Six', 
+            'Seven', 'Eight', 'Nine', 'Ten', 'Eleven', 'Twelve', 
+            'Thirteen', 'Fourteen', 'Fifteen', 'Sixteen',
             'Seventeen', 'Eighteen', 'Nineteen'
         ]
 
         tens = [
-            '', '', 'Twenty', 'Thirty', 'Forty', 'Fifty', 'Sixty', 'Seventy', 'Eighty', 'Ninety'
+            '', '', 'Twenty', 'Thirty', 'Forty', 'Fifty', 
+            'Sixty', 'Seventy', 'Eighty', 'Ninety'
         ]
 
         thousands = [ 
-            (1_000_000_000, 'Billion'), (1_000_000, 'Million'), (1_000, 'Thousand')
+            (1_000_000_000, 'Billion'), 
+            (1_000_000, 'Million'), 
+            (1_000, 'Thousand')
         ]
 
         def translate(num):
-            """ Translate numbers less than 1000 """
             if num == 0:
                 return ''
             elif num < 20:
                 return lt20[num]
             elif num < 100:
                 q, r = divmod(num, 10)
-                return tens[q] + ' ' + translate(r)
+                return tens[q] + (' ' + translate(r) if r else '')
             else:
-                q, r  = divmod(num, 100)
-                return lt20[q] + ' Hundred ' + translate(r)
+                q, r = divmod(num, 100)
+                return lt20[q] + ' Hundred' + (' ' + translate(r) if r else '')
 
         res = []
-
+        
         for factor, unit in thousands:
-            if num // factor != 0:
+            if num >= factor:
                 quotient, num = divmod(num, factor)
-                res.append(translate(quotient).strip())
+                res.append(translate(quotient))
                 res.append(unit)
         
         if num > 0:
-            res.append(translate(num).strip())
+            res.append(translate(num))
 
-        return ' '.join(res) if res else lt20[num]
-
-        
+        return ' '.join(res)
