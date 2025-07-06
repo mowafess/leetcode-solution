df31103e416921e692af17c14eb37454cb6173e6
Mowa O <36759376+mowafess@users.noreply.github.com>
2025-07-06 21:45:29 +0100

    Time: 3 ms (26.34%), Space: 18 MB (25.55%) - LeetHub

--- 0273-integer-to-english-words/0273-integer-to-english-words.py
+++ 0273-integer-to-english-words/0273-integer-to-english-words.py
@@ -1,47 +1,82 @@
+# class Solution:
+#     def numberToWords(self, num: int) -> str:
+#         if num == 0:
+#             return "Zero"
+        
+#         lt20 = [
+#             '', 'One', 'Two', 'Three', 'Four', 'Five', 'Six', 
+#             'Seven', 'Eight', 'Nine', 'Ten', 'Eleven', 'Twelve', 
+#             'Thirteen', 'Fourteen', 'Fifteen', 'Sixteen',
+#             'Seventeen', 'Eighteen', 'Nineteen'
+#         ]
+
+#         tens = [
+#             '', '', 'Twenty', 'Thirty', 'Forty', 'Fifty', 
+#             'Sixty', 'Seventy', 'Eighty', 'Ninety'
+#         ]
+
+#         thousands = [ 
+#             (1_000_000_000, 'Billion'), 
+#             (1_000_000, 'Million'), 
+#             (1_000, 'Thousand')
+#         ]
+
+#         def translate(num):
+#             if num == 0:
+#                 return ''
+#             elif num < 20:
+#                 return lt20[num]
+#             elif num < 100:
+#                 q, r = divmod(num, 10)
+#                 return tens[q] + (' ' + translate(r) if r else '')
+#             else:
+#                 q, r = divmod(num, 100)
+#                 return lt20[q] + ' Hundred' + (' ' + translate(r) if r else '')
+
+#         res = []
+        
+#         for factor, unit in thousands:
+#             if num >= factor:
+#                 quotient, num = divmod(num, factor)
+#                 res.append(translate(quotient))
+#                 res.append(unit)
+        
+#         if num > 0:
+#             res.append(translate(num))
+
+#         return ' '.join(res)
+
+
 class Solution:
     def numberToWords(self, num: int) -> str:
         if num == 0:
             return "Zero"
         
-        lt20 = [
-            '', 'One', 'Two', 'Three', 'Four', 'Five', 'Six', 
-            'Seven', 'Eight', 'Nine', 'Ten', 'Eleven', 'Twelve', 
-            'Thirteen', 'Fourteen', 'Fifteen', 'Sixteen',
-            'Seventeen', 'Eighteen', 'Nineteen'
-        ]
-
-        tens = [
-            '', '', 'Twenty', 'Thirty', 'Forty', 'Fifty', 
-            'Sixty', 'Seventy', 'Eighty', 'Ninety'
-        ]
-
-        thousands = [ 
-            (1_000_000_000, 'Billion'), 
-            (1_000_000, 'Million'), 
-            (1_000, 'Thousand')
-        ]
-
-        def translate(num):
-            if num == 0:
-                return ''
-            elif num < 20:
-                return lt20[num]
-            elif num < 100:
-                q, r = divmod(num, 10)
-                return tens[q] + (' ' + translate(r) if r else '')
+        # All unique number words
+        words = {
+            1: "One", 2: "Two", 3: "Three", 4: "Four", 5: "Five",
+            6: "Six", 7: "Seven", 8: "Eight", 9: "Nine", 10: "Ten",
+            11: "Eleven", 12: "Twelve", 13: "Thirteen", 14: "Fourteen", 15: "Fifteen",
+            16: "Sixteen", 17: "Seventeen", 18: "Eighteen", 19: "Nineteen", 20: "Twenty",
+            30: "Thirty", 40: "Forty", 50: "Fifty", 60: "Sixty", 70: "Seventy",
+            80: "Eighty", 90: "Ninety", 100: "Hundred", 1000: "Thousand",
+            1000000: "Million", 1000000000: "Billion"
+        }
+        
+        def helper(n):
+            if n == 0:
+                return []
+            elif n < 20:
+                return [words[n]]
+            elif n < 100:
+                return [words[n // 10 * 10]] + helper(n % 10)
+            elif n < 1000:
+                return [words[n // 100], "Hundred"] + helper(n % 100)
+            elif n < 1000000:
+                return helper(n // 1000) + ["Thousand"] + helper(n % 1000)
+            elif n < 1000000000:
+                return helper(n // 1000000) + ["Million"] + helper(n % 1000000)
             else:
-                q, r = divmod(num, 100)
-                return lt20[q] + ' Hundred' + (' ' + translate(r) if r else '')
-
-        res = []
+                return helper(n // 1000000000) + ["Billion"] + helper(n % 1000000000)
         
-        for factor, unit in thousands:
-            if num >= factor:
-                quotient, num = divmod(num, factor)
-                res.append(translate(quotient))
-                res.append(unit)
-        
-        if num > 0:
-            res.append(translate(num))
-
-        return ' '.join(res)
+        return " ".join(helper(num))
