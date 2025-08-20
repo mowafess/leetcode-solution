52e28c5ea84ea4ae3d8555ec9e56b77131bf9cde
Mowa O <36759376+mowafess@users.noreply.github.com>
2025-08-20 16:58:13 +0100

    Time: 75 ms (48.25%), Space: 22.7 MB (51.84%) - LeetHub

---
+++ 1014-k-closest-points-to-origin/1014-k-closest-points-to-origin.py
@@ -0,0 +1,18 @@
+from heapq import heappush, heappop
+
+class Solution:
+    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+        store = []
+        
+        # N log N
+        for i, point in enumerate(points):
+            heappush(store, (math.hypot(*point), points[i]))
+            
+        output = []
+        while k > 0:
+            dist, point = heappop(store)
+            output.append(point)
+            k -= 1
+            
+        return output
+    
