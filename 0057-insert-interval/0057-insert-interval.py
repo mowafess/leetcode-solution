7dea4ad5c49c69dba0c966d80ac0320dcc8a2e08
Mowa O <36759376+mowafess@users.noreply.github.com>
2025-10-08 22:31:38 +0100

    Time: 3 ms (50.16%), Space: 19.9 MB (19.55%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.py
+++ 0057-insert-interval/0057-insert-interval.py
@@ -9,16 +9,12 @@ class Solution:
             if end < newInterval[0]:
                 out.append([start, end])
                 i += 1
-            else:
-                break
-        # print(i)
-        # left = i
+
+
         for start, end in intervals[i:]:
             if start <= newInterval[1]:
                 newInterval = [min(start, newInterval[0]), max(end, newInterval[1])]
                 i += 1
-            else:
-                break
 
         out.append(newInterval)
         
