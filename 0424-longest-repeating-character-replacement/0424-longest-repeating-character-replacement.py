86c94b6790b240c5d173733738503f3f24bbaf48
Mowa O <36759376+mowafess@users.noreply.github.com>
2025-10-22 13:37:00 +0100

    Time: 116 ms (65.95%), Space: 17.9 MB (97.46%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -1,37 +1,21 @@
-class Solution(object):
-    def characterReplacement(self, s, k):
-        """
-        :type s: str
-        :type k: int
-        :rtype: int
-        """
-#         l, longest = 0, 0
-#         cache = collections.Counter()
-        
-#         for r in range(len(s)):
-#             cache[s[r]] += 1
-            
-#             while (r - l + 1) - cache.most_common(1)[0][1] > k:
-#                 cache[s[l]] -= 1
-#                 l += 1
-            
-#             longest = max(longest, r - l + 1)
-            
-        
-#         return longest
-    
-        l, longest, max_freq = 0, 0, 0
-        cache = collections.Counter()
-        
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        l = 0
+        state = {}
+        out = 0
+        max_freq = 0
+
+
         for r in range(len(s)):
-            cache[s[r]] += 1
-            max_freq = max(max_freq, cache[s[r]])
+            state[s[r]] = state.get(s[r], 0) + 1
+            max_freq = max(max_freq, state[s[r]])
             
+            # window_size = (r - l + 1)
             while (r - l + 1) - max_freq > k:
-                cache[s[l]] -= 1
+                state[s[l]] -= 1
                 l += 1
-            
-            longest = max(longest, r - l + 1)
-            
+
+            out = max(out, r - l + 1)
+
+        return out
         
-        return longest
