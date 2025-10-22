7baf532220ed24221d7a25ac64dffd8dd5b67b85
Mowa O <36759376+mowafess@users.noreply.github.com>
2025-10-22 13:21:56 +0100

    Time: 19 ms (53.67%), Space: 18 MB (12.28%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -1,42 +1,19 @@
-class Solution(object):
-    def lengthOfLongestSubstring(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
-        
-        cache = set()
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
         l = 0
-        longest = 0
-        
+        out = 0
+        state = set()
+
         for r in range(len(s)):
-            while s[r] in cache:
-                cache.remove(s[l])
+            while s[r] in state:
+                state.remove(s[l])
                 l += 1
             
-            cache.add(s[r])
-            
-            longest = max(longest, r - l + 1)
-            
-        return longest
+            state.add(s[r])
+
+            out = max(out, r - l + 1)
+
         
-        cache = collections.defaultdict(int)
-        
-        l, r, longest = 0, 0, 0
-        
-        while r < len(s):
-            # shrink window if new item (r) already exist
-            while cache[s[r]] and l < r:
-                cache[s[l]] -= 1
-                l += 1
-            
-            # else expand window
-            cache[s[r]] += 1
-            r += 1
-            
-            longest = max(longest, r - l)
-            
-        
-        return longest
-            
+        return out
+
         
